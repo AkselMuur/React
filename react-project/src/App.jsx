489cb70ae4809daaf00eeda1c8a0a4edcc9ea361
Paan Kaur <paan.kaur@gmail.com>
2026-01-07 11:39:50 +0200

    Ülesanne 2

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,24 +1,22 @@
-import ExpenseItem from "./components/ExpenseItem.jsx";
-import "./components/ExpenseItem.css";
+import Expenses from "./components/Expenses.jsx";
 
 const App = () => {
   const expenses = [
     {
-    date: new Date(2024, 10, 12),
-    title: 'New book1',
-    price: 30.99
-  },
-  {
-    date: new Date(2025, 7, 12),
-    title: 'New book2',
-    price: 99.30
-  }
-]
+      date: new Date(2024, 10, 12),
+      title: "New book1",
+      price: 30.99,
+    },
+    {
+      date: new Date(2025, 7, 12),
+      title: "New book2",
+      price: 99.3,
+    },
+  ];
 
   return (
     <div className="App">
-      <ExpenseItem data={expenses[0]} />
-            <ExpenseItem data={expenses[1]} />
+      <Expenses expenses={expenses} />
     </div>
   );
 };
--- react-project/src/components/ExpenseItem.jsx
+++ react-project/src/components/ExpenseItem.jsx
@@ -6,10 +6,10 @@ const ExpenseItem = (props) => {
   
   return (
     <div className="expense-item">
-    <ExpenseDate date={props.data.date}/>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{props.data.title}</h2>
-        <div className="expense-item__price">{props.data.price}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.price}</div>
       </div>
     </div>
   );

0a2f6ba28e28c635a517aa3b771fbcf64dfdf9fe
Paan Kaur <paan.kaur@gmail.com>
2026-01-08 12:42:33 +0200

    7. Sündmuste töötlus

--- react-project/src/components/Expenses/ExpenseItem.jsx
+++ react-project/src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,24 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card.jsx";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  
+  const [title, setTitel] = useState(props.title);
+
+  const clickHandler = () => {
+    console.log("Clicked");
+    setTitel(`Updated by click ${title}`);
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
-       <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.price}</div>
       </div>
+      <button onClick={clickHandler}>Click Me</button>
     </Card>
   );
 };
